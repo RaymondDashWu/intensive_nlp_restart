cdffdbcec2e8c230a0c9c2ed1d05224a0f155f7f
RaymondDashWu <raymond31670@hotmail.com>
2019-03-24 21:13:53 -0700

    [ADD] requirements + clientJS

--- app/static/client.js
+++ app/static/client.js
@@ -1,20 +1,24 @@
 var el = x => document.getElementById(x);
 
-function showPicker(inputId) { el('file-input').click(); }
+// function showPicker(inputId) { el('file-input').click(); }
 
 function showPicked(input) {
-    el('upload-label').innerHTML = input.files[0].name;
+    // el('upload-label').innerHTML = input.files[0].name;
+    el('upload-label').innerHTML = input.name;
     var reader = new FileReader();
     reader.onload = function (e) {
         el('image-picked').src = e.target.result;
         el('image-picked').className = '';
     }
-    reader.readAsDataURL(input.files[0]);
+    // reader.readAsDataURL(input.files[0]);
+    reader.readAsText(input);
 }
 
 function analyze() {
-    var uploadFiles = el('file-input').files;
-    if (uploadFiles.length != 1) alert('Please select 1 file to analyze!');
+    var uploadFiles = el('reviewText');
+    // var uploadFiles = el('file-input').files;
+
+    // if (uploadFiles.length != 1) alert('Please select 1 file to analyze!');
 
     el('analyze-button').innerHTML = 'Analyzing...';
     var xhr = new XMLHttpRequest();
@@ -23,6 +27,7 @@ function analyze() {
     xhr.onerror = function() {alert (xhr.responseText);}
     xhr.onload = function(e) {
         if (this.readyState === 4) {
+            console.log("e.target:", e.target)
             var response = JSON.parse(e.target.responseText);
             el('result-label').innerHTML = `Result = ${response['result']}`;
         }
@@ -30,7 +35,9 @@ function analyze() {
     }
 
     var fileData = new FormData();
-    fileData.append('file', uploadFiles[0]);
+    fileData.append('file', uploadFiles);
+    console.log("fileData:", fileData);
+    console.log("uploadFiles:", uploadFiles);
     xhr.send(fileData);
 }
 
