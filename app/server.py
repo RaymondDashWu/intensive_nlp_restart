a91147578fafbcdb54367946d046c50afcaa36a7
RaymondDashWu <raymond31670@hotmail.com>
2019-03-24 23:21:15 -0700

    [TEST] BytesIO vs String

--- app/server.py
+++ app/server.py
@@ -59,7 +59,7 @@ async def analyze(request):
     # img_bytes = await (data['file'].read())
     # took out img_bytes
     # img = open_image(BytesIO(img_bytes))
-    img = StringIO(data["textField"])
+    img = BytesIO(data["textField"])
     print("img:", img)
     # prediction = learn.predict(img)[0]
     prediction = learn.predict(img)
