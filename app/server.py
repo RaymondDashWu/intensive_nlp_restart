7cb47bb7e85c7834aed4db491e9944a7e9819cb6
RaymondDashWu <raymond31670@hotmail.com>
2019-03-25 00:52:12 -0700

    [TEST] revert bytes to string

--- app/server.py
+++ app/server.py
@@ -59,7 +59,7 @@ async def analyze(request):
     # img_bytes = await (data['file'].read())
     # took out img_bytes
     # img = open_image(BytesIO(img_bytes))
-    img = BytesIO(data["textField"])
+    img = StringIO(data["textField"])
     print("img:", img)
     # prediction = learn.predict(img)[0]
     prediction = learn.predict(img)
